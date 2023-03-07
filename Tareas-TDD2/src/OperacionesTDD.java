2e80d5259992502f98b0d533803d72eaf9315221
billymaynard <billymaynardire@gmail.com>
2023-03-07 23:51:21 +0100

    Revert "Segundo Ejercicio Commit del B"
    This reverts commit c0b2ff245f4f4c11808fe1ef8b1d82a359307b2e.

--- Tareas-TDD2/src/OperacionesTDD.java
+++ Tareas-TDD2/src/OperacionesTDD.java
@@ -3,13 +3,6 @@ public class OperacionesTDD {
         if (nums.length()==0){
             return 0;
         }
-        int intof=0;
-        for (int i = 0; i < nums.length();i++){
-            String sub = nums.substring(i,i+1);
-            if (!sub.equals(",")){
-                intof= intof+Integer.valueOf(sub);
-            }
-        }
-        return intof;
+        return Integer.valueOf(nums);
     }
 }
