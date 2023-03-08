a61ed3add763975fa73ad1fddba01ef37fa4d706
billymaynard <billymaynardire@gmail.com>
2023-03-08 04:22:18 +0100

    Se Añade Comentarios de Arrange, Act y Assert.

--- Tareas-TDD2/Tests/OperacionesTDDTest.java
+++ Tareas-TDD2/Tests/OperacionesTDDTest.java
@@ -5,44 +5,65 @@ import org.junit.jupiter.api.Test;
 class OperacionesTDDTest {
     @Test
     void TestAdelPunto4(){
+        //Arrange
         OperacionesTDD test = new OperacionesTDD();
+        //Act
         int respuesta = test.suma("");
+        //Assert
         Assertions.assertEquals(0,respuesta);
     }
     @Test
     void TestBdelPunto4(){
+        //Arrange
         OperacionesTDD test = new OperacionesTDD();
+        //Act
         int respuesta = test.suma("1");
+        //Assert
         Assertions.assertEquals(1,respuesta);
     }
     @Test
     void TestCdelPunto4(){
+        //Arrange
         OperacionesTDD test = new OperacionesTDD();
+        //Act
         int respuesta = test.suma("1,2");
+        //Assert
         Assertions.assertEquals(3,respuesta);
     }
     @Test
     void TestDdelPunto4(){
+        //Arrange
         OperacionesTDD test = new OperacionesTDD();
+        //Act
         int respuesta = test.suma("1,1,2");
+        //Assert
         Assertions.assertEquals(4,respuesta);
     }
     @Test
     void TestAdelPunto5(){
+        //Arrange
         OperacionesTDD test = new OperacionesTDD();
+        //Act
         int respuesta = test.suma("1,2,");
+        //Assert
         Assertions.assertEquals(-1,respuesta);
     }
     @Test
     void TestAdelPunto6(){
+        //Arrange
         OperacionesTDD test = new OperacionesTDD();
+        //Act
         int respuesta = test.suma("1,2,-2");
+        //Assert
         Assertions.assertEquals(-1,respuesta);
     }
     @Test
     void TestAdelPunto7(){
+        //Arrange
         OperacionesTDD test = new OperacionesTDD();
+        //Act
         int respuesta = test.suma("5,1002,7");
+        //Assert
         Assertions.assertEquals(12,respuesta);
     }
 }
